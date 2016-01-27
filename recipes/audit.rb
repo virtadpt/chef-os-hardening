d209676b75ddef901e6f699cacc94df2c7f50acd
The Doctor <drwho@virtadpt.net>
2016-01-27 14:32:04 -0800

    Added auditd configuration files.
    Trying to revert how badly screwed up I made the /etc/motd file.
    
    Added a cookbook to install and configure auditd.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

---
+++ recipes/audit.rb
@@ -0,0 +1,21 @@
+
+package 'audit'
+
+cookbook_file '/etc/audit/audit.rules' do
+    source 'audit.rules'
+end
+
+cookbook_file '/etc/audit/auditd.conf' do
+    source 'auditd.conf'
+end
+
+execute 'generate_audit_rules' do
+    command 'find / -xdev \( -perm -4000 -o -perm -2000 \) -type f | awk \'{print "-a always,exit -F path=" $1 " -F perm=x -F auid>=500 -F auid!=4294967295 -k privileged"}\' >> /etc/audit/audit.rules'
+    creates '/etc/audit/.auditd.configured'
+end
+
+execute 'make_auditd_rules_immutable' do
+    command 'echo "-e 2" >> /etc/audit.rules'
+    creates '/etc/audit/.auditd.locked'
+end
+
