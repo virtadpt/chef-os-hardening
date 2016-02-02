174a282de44037077900402e7b2a3c81cca89ea7
The Doctor <drwho@virtadpt.net>
2016-02-01 16:34:08 -0800

    Added an sshd_config file for RHEL7 and its derivatives.
    Working on a set of conditionals to decide which sshd_configuration file to
    install on which kind of system.  Right now I'm only working on RHEL/Centos
    v7.x.
    
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- recipes/ssh.rb
+++ recipes/ssh.rb
@@ -1,9 +1,11 @@
 
 cookbook_file '/etc/ssh/sshd_config' do
-    source 'sshd_config'
+    source 'sshd_config.rhel7'
     mode '0600'
     owner 'root'
     group 'root'
+    only_if { platform_family?('rhel') || platform_family('centos')}
+    only_if { node['platform_version'].to_f >= 7.0 }
 end
 
 file '/etc/ssh/sshd_banner' do
